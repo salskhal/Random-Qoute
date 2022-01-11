b411a00780d4860689159f7616076f2a2137f7ba
Khalid Salman-Yusuf <olaniyisal@gmail.com>
2022-01-11 09:07:50 +0100

    Feature/ Changed API and implemented Random

--- main.js
+++ main.js
@@ -1,20 +1,31 @@
+"use strict"
 const quoteBtn = document.querySelector(".btn")
-const API = "https://breaking-bad-quotes.herokuapp.com/v1/quotes"
-
+const api = "https://type.fit/api/quotes"
 
 quoteBtn.addEventListener("click", fetchQuote)
 
-function fetchQuote(){
-    fetch(API)
-    .then(res => res.json())
-    .then(quotes => {
-        quotes.map(quote => {
+// const API = "https://breaking-bad-quotes.herokuapp.com/v1/quotes"
+
+// function fetchQuote(){
+//     fetch(API)
+//     .then(res => res.json())
+//     .then(quotes => {
+//         quotes.map(quote => {
+//             document.querySelector(".quote").classList.remove("hidden")
+//             document.querySelector(".quote").innerHTML = ` <h3 class="main-quote">${quote.quote}</h3>
+//             <p class="author">--${quote.author}</p>`
+//         })
+//     })
+// }
+
+function fetchQuote() {
+    fetch(api)
+        .then(res => res.json())
+        .then(quotes => {
+            let randomQuote = Math.trunc(Math.random() * quotes.length) + 1
+            let currentQuote = quotes[randomQuote] 
             document.querySelector(".quote").classList.remove("hidden")
-            document.querySelector(".quote").innerHTML = ` <h3 class="main-quote">${quote.quote}</h3>
-            <p class="author">--${quote.author}</p>`
+            document.querySelector(".quote").innerHTML = ` <h3 class="main-quote">${currentQuote.text}</h3>
+            <p class="author">--${currentQuote.author}</p>`
         })
-    })
 }
-
-
-
